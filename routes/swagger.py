5b76c01807001a90b59598619fc49e784d619df0
A'zamov Samandar <azamov.samandar.programmer@gmail.com>
2024-04-04 15:14:51 +0500

    update

--- routes/swagger.py
+++ routes/swagger.py
@@ -13,7 +13,7 @@ schema_view = get_schema_view(
         default_version='v1',
         description="Test description",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
+        contact=openapi.Contact(email="JscorpTech@gmail.com"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
