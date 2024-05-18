8caff33317ebe5d80f03eff37608de1bdccd9efb
Azamov Samandar <JscorpTech@gmail.com>
2024-05-19 04:38:50 +0500

    Removed drf-yasg and install drf-spectacular

--- core/apps/accounts/views/change_password.py
+++ core/apps/accounts/views/change_password.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.hashers import make_password
 
-from drf_yasg.utils import swagger_auto_schema
-from drf_yasg import openapi
 
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import ValidationError
 from rest_framework import status
+from drf_spectacular.utils import (
+    extend_schema,
+    OpenApiResponse,
+)
 
 from core.http import views as http_views
 from ..serializers import ChangePasswordSerializer
@@ -14,17 +16,16 @@ from ..serializers import ChangePasswordSerializer
 
 class ChangePasswordView(APIView, http_views.ApiResponse):
     """usaer password change view"""
+
     serializer_class = ChangePasswordSerializer
     permission_classes = (IsAuthenticated,)
 
-    @swagger_auto_schema(
-        request_body=serializer_class,
-        responses={
-            status.HTTP_200_OK: openapi.Response("Password changed successfully"),
-            status.HTTP_400_BAD_REQUEST: openapi.Response("Bad request")
-        },
-        operation_summary="Change user password.",
-        operation_description="Change password of the authenticated user.")
+    @extend_schema(
+        request=serializer_class,
+        responses={200: OpenApiResponse(ChangePasswordSerializer)},
+        summary="Change user password.",
+        description="Change password of the authenticated user.",
+    )
     def post(self, request, *args, **kwargs):
         user = self.request.user
         if user is None:
@@ -36,9 +37,15 @@ class ChangePasswordView(APIView, http_views.ApiResponse):
             )
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
-            if user.check_password(request.data['old_password']):
-                user.password = make_password(request.data['new_password'])
+            if user.check_password(request.data["old_password"]):
+                user.password = make_password(request.data["new_password"])
                 user.save()
-                return http_views.ApiResponse().success("password changed successfully", status_code=status.HTTP_200_OK)
-            return http_views.ApiResponse().error("wrong old password", status_code=status.HTTP_400_BAD_REQUEST)
-        return http_views.ApiResponse().error(serializer.errors, status_code=status.HTTP_400_BAD_REQUEST)
+                return http_views.ApiResponse().success(
+                    "password changed successfully", status_code=status.HTTP_200_OK
+                )
+            return http_views.ApiResponse().error(
+                "wrong old password", status_code=status.HTTP_400_BAD_REQUEST
+            )
+        return http_views.ApiResponse().error(
+            serializer.errors, status_code=status.HTTP_400_BAD_REQUEST
+        )
