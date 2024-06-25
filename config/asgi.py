9b4028f4645c8d31cf02f50fe0de0e1ac4f13843
A'zamov Samandar <JscorpTech@gmail.com>
2024-06-25 14:46:44 +0500

    Created websocket

--- config/asgi.py
+++ config/asgi.py
@@ -1,9 +1,19 @@
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import URLRouter
+from core.apps.home.websocket.routing import urlpatterns
 
 from common.env import env
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", env("DJANGO_SETTINGS_MODULE"))
 
-application = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(URLRouter(urlpatterns))
+    }
+)
