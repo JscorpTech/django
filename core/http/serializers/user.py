7a06e4529a161d10fe4b449963f802dcafd20586
A'zamov Samandar <JscorpTech@gmail.com>
2024-07-10 12:46:10 +0500

    Add minimum length validation for new password field

--- core/http/serializers/user.py
+++ core/http/serializers/user.py
@@ -7,14 +7,3 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ["first_name", "last_name", "phone"]
         model = models.User
-
-    def update(self, instance, validated_data):
-        instance.first_name = validated_data.get(
-            "first_name", instance.first_name
-        )
-        instance.last_name = validated_data.get(
-            "last_name", instance.last_name
-        )
-        instance.phone = validated_data.get("phone", instance.phone)
-        instance.save()
-        return instance
