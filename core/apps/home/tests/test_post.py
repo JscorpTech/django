fd5a0d10d98e86aeaa40b271d818f856019e725a
A'zamov Samandar <JscorpTech@gmail.com>
2024-07-10 13:56:26 +0500

    Add ignore rules for flake8 and test translation models

--- core/apps/home/tests/test_post.py
+++ core/apps/home/tests/test_post.py
@@ -1,10 +1,42 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
+from pydantic import BaseModel, RootModel
+from typing import List
+import logging
+from core.http.models import Post
+
+
+class PostsModel(BaseModel):
+    title: str
+    desc: str
+    image: str
+
+
+class PostsListModel(RootModel[List[PostsModel]]): ...
 
 
 class Test(TestCase):
 
     def setUp(self) -> None:
-        return super().setUp()
+        self.clien = APIClient()
+        self.post = Post.objects.create(
+            title="title", desc="desc", image="image.jpg"
+        )
 
-    def get_posts(self) -> None:
-        self.assertTrue(True)
+    def test_get_posts(self) -> None:
+        response = self.clien.get(reverse("posts-list"))
+        self.assertEqual(response.status_code, 200)
+        logging.info(response.json())
+        try:
+            PostsListModel(response.json())
+        except Exception:
+            self.fail("Response is not PostsListModel")
+
+    def test_post_detail(self):
+        response = self.clien.get(reverse("posts-detail", args=[self.post.id]))
+        self.assertEqual(response.status_code, 200)
+        try:
+            PostsModel(**response.json())
+        except Exception:
+            self.fail("Response is not PostsModel")
