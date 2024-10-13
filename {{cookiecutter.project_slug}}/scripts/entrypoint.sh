6513ec35fa275cd66937bd889591bb86c7bcbf21
A'zamov Samandar <JscorpTech@gmail.com>
2024-10-13 20:20:37 +0500

    poetry remove

--- {{cookiecutter.project_slug}}/scripts/entrypoint.sh
+++ {{cookiecutter.project_slug}}/scripts/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-poetry run python3 manage.py migrate --noinput
-poetry run python3 manage.py collectstatic --noinput
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput
 
 {% if cookiecutter.runner == 'asgi' %}
-poetry run daphne -p 8000 -b 0.0.0.0 config.asgi:application
+daphne -p 8000 -b 0.0.0.0 config.asgi:application
 {% else %}
-poetry run python3 manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:8000
 {% endif %}
 
 {% if cookiecutter.celery == 'y' %}
-& poetry run celery -A config worker --loglevel=info &
-sleep 10 && poetry run celery -A config flower --loglevel=info
+& celery -A config worker --loglevel=info &
+sleep 10 && celery -A config flower --loglevel=info
 {% endif %}
 
 exit $?
