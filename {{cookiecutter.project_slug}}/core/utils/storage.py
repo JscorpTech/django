cb80bb50e5c6e5fbfd96d531cda80c427c3eb316
A'zamov Samandar <JscorpTech@gmail.com>
2024-11-01 19:17:45 +0500

    Fixed storage issues

--- {{cookiecutter.project_slug}}/core/utils/storage.py
+++ {{cookiecutter.project_slug}}/core/utils/storage.py
@@ -1,23 +1,15 @@
-from config.env import env
-from typing import Optional
+from typing import Optional, Union
 
 
 class Storage:
 
-    def __init__(self) -> None:
-        self.storage = env.str("STORAGE", "FILE")
-
-    def get(self, key, default=None) -> Optional[str]:
-        data = {}
-        match self.storage:
+    def get_backend(self, storage_type: Union[str]) -> Optional[str]:
+        match storage_type:
             case "AWS" | "MINIO":
-                data = {
-                    "default": "storages.backends.s3boto3.S3Boto3Storage",
-                    "static": "storages.backends.s3boto3.S3Boto3Storage",
-                }
+                return "storages.backends.s3boto3.S3Boto3Storage"
             case "FILE":
-                data = {
-                    "default": "django.core.files.storage.FileSystemStorage",
-                    "static": "django.contrib.staticfiles.storage.StaticFilesStorage",
-                }
-        return data.get(key, default)
+                return "django.core.files.storage.FileSystemStorage"
+            case "STATIC":
+                return "django.contrib.staticfiles.storage.StaticFilesStorage"
+            case _:
+                raise TypeError(f"{storage_type} is not supported")
