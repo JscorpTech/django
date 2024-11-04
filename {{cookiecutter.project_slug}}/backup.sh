9b3f1082835177ec43ad7a873b95bed20d41e55b
Samandar Azamov <jscorptech@gmail.com>
2024-11-04 07:07:54 +0000

    Update backup.sh

--- {{cookiecutter.project_slug}}/backup.sh
+++ {{cookiecutter.project_slug}}/backup.sh
@@ -1,5 +1,5 @@
 file=/tmp/db-$(/usr/bin/date +\%Y-%m-%d-%H:%M:%S).sql
-container=9560b8175cfd
+container=postgres
 /usr/bin/docker container exec $container pg_dump -U postgres django > $file
-scp $file /home/backup/db/document/
+mc cp $file b2/buket-name
 rm $file
