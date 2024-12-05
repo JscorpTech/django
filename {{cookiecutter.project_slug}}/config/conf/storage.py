fe01f94cb6087b7f2f729285fadb0dad77b38269
A'zamov Samandar <JscorpTech@gmail.com>
2024-12-05 14:06:24 +0500

    Add storage configuration to .env and update storage.py

--- {{cookiecutter.project_slug}}/config/conf/storage.py
+++ {{cookiecutter.project_slug}}/config/conf/storage.py
@@ -4,7 +4,8 @@ from core.utils.storage import Storage
 AWS_ACCESS_KEY_ID = env.str("STORAGE_ID")
 AWS_SECRET_ACCESS_KEY = env.str("STORAGE_KEY")
 AWS_S3_ENDPOINT_URL = env.str("STORAGE_URL")
-
+AWS_S3_CUSTOM_DOMAIN = env.str("STORAGE_PATH")
+AWS_S3_URL_PROTOCOL = env.str("STORAGE_PROTOCOL", "https:")
 
 default_storage = Storage(env.str("STORAGE_DEFAULT"), "default")
 static_storage = Storage(env.str("STORAGE_STATIC"), "static")
