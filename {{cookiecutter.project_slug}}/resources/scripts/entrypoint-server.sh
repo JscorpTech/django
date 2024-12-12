d1bf174ba2ccc71704b04c18e728645f021bfc56
A'zamov Samandar <JscorpTech@gmail.com>
2024-12-12 17:22:42 +0500

    Ishga tushuruchi script yangilandi

---
+++ {{cookiecutter.project_slug}}/resources/scripts/entrypoint-server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+python3 manage.py collectstatic --noinput
+python3 manage.py migrate --noinput
+
+gunicorn config.wsgi:application -b 0.0.0.0:8000 --workers $(($(nproc) * 2 + 1))
+
+{% if cookiecutter.celery == 'y' %}
+& celery -A config worker --loglevel=info &
+sleep 10 && celery -A config flower --loglevel=info
+{% endif %}
+
+exit $?
+
+
