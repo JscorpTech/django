4de4e3687c9f8ab482272da92b98dcb2e4f3d7e6
A'zamov Samandar <JscorpTech@gmail.com>
2024-12-12 17:35:01 +0500

    throttle request count update

--- {{cookiecutter.project_slug}}/config/settings/local.py
+++ {{cookiecutter.project_slug}}/config/settings/local.py
@@ -6,5 +6,5 @@ INSTALLED_APPS += ["django_extensions"]
 ALLOWED_HOSTS += ["127.0.0.1", "192.168.100.26"]
 
 REST_FRAMEWORK["DEFAULT_THROTTLE_RATES"] = {
-    "user": "10/min",
+    "user": "60/min",
 }
--- {{cookiecutter.project_slug}}/config/settings/production.py
+++ {{cookiecutter.project_slug}}/config/settings/production.py
@@ -3,4 +3,4 @@ from config.settings.common import ALLOWED_HOSTS, REST_FRAMEWORK
 
 ALLOWED_HOSTS += ["192.168.100.26", "80.90.178.156"]
 
-REST_FRAMEWORK["DEFAULT_THROTTLE_RATES"] = {"user": "10/min"}
+REST_FRAMEWORK["DEFAULT_THROTTLE_RATES"] = {"user": "60/min"}
