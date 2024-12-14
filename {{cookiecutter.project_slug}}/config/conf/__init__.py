940242309d0bd8cf730c6f90998cdd6fe24d9d90
A'zamov Samandar <JscorpTech@gmail.com>
2024-12-14 15:32:52 +0500

    `#spectacular modulini import qildim`

--- {{cookiecutter.project_slug}}/config/conf/__init__.py
+++ {{cookiecutter.project_slug}}/config/conf/__init__.py
@@ -5,6 +5,7 @@ from .jwt import *  # noqa
 from .logs import *  # noqa
 from .rest_framework import *  # noqa
 from .unfold import *  # noqa
+from .spectacular import * # noqa
 
 {% if cookiecutter.ckeditor %}from .ckeditor import *  # noqa{% endif %}
 {% if cookiecutter.storage %}from .storage import *  # noqa{% endif %}
