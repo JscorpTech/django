d60221bb0b7c6149a634f750aef33f0ac785192a
A'zamov Samandar <JscorpTech@gmail.com>
2024-12-18 18:17:52 +0500

    ` "Cache: funksiyaga timeout va args/kwargs qo'shildi, keshni faollashtirish shartlari o'zgartirildi"`

--- {{cookiecutter.project_slug}}/core/utils/cache.py
+++ {{cookiecutter.project_slug}}/core/utils/cache.py
@@ -1,17 +1,18 @@
 import hashlib
 
-from config.env import env
 from django.core.cache import cache
+
+from config.env import env
 
 
 class Cache:
-    def remember(self, func, key: str):
-        cache_enabled = env("CACHE_ENABLED")
+    def remember(self, func, key: str, timeout=None, *args, **kwargs):
+        cache_enabled = env.bool("CACHE_ENABLED")
         key = hashlib.md5(key.encode("utf-8")).hexdigest()
         response = cache.get(key)
-
-        if (response is None) or cache_enabled:
-            response = func()
-            cache.set(key, response, env("CACHE_TIME"))
-
+        if not cache_enabled:
+            return func(*args, **kwargs)
+        elif response is None:
+            response = func(*args, **kwargs)
+            cache.set(key, response, env.int("CACHE_TIME") if timeout is None else timeout)
         return response
