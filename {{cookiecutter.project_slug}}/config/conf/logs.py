8ece4f6659942daef068f9974fc2d19bb5b689d0
A'zamov Samandar <admin@jscorp.uz>
2025-10-13 19:38:14 +0500

    feat: logging

--- {{cookiecutter.project_slug}}/config/conf/logs.py
+++ {{cookiecutter.project_slug}}/config/conf/logs.py
@@ -1,4 +1,17 @@
-# settings.py faylida
+import os
+from pathlib import Path
+import logging
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+LOG_DIR = BASE_DIR / "resources/logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+
+
+class ExcludeErrorsFilter:
+    def filter(self, record):
+        return record.levelno <= logging.ERROR
+
 
 LOGGING = {
     "version": 1,
@@ -9,19 +22,38 @@ LOGGING = {
             "style": "{",
         },
     },
+    "filters": {
+        "exclude_errors": {
+            "()": ExcludeErrorsFilter,
+        },
+    },
     "handlers": {
         "daily_rotating_file": {
             "level": "INFO",
             "class": "logging.handlers.TimedRotatingFileHandler",
-            "filename": "resources/logs/django.log",  # Fayl nomi (kunlik fayllar uchun avtomatik yoziladi)
-            "when": "midnight",  # Har kecha log fayli yangilanadi
-            "backupCount": 30,  # 30 kunlik loglar saqlanadi, 1 oydan keyin eski fayllar o'chiriladi
+            "filename": LOG_DIR / "django.log",
+            "when": "midnight",
+            "backupCount": 30,
+            "formatter": "verbose",
+            "filters": ["exclude_errors"],
+        },
+        "error_file": {
+            "level": "ERROR",
+            "class": "logging.handlers.TimedRotatingFileHandler",
+            "filename": LOG_DIR / "django_error.log",
+            "when": "midnight",
+            "backupCount": 30,
             "formatter": "verbose",
         },
     },
     "loggers": {
         "django": {
-            "handlers": ["daily_rotating_file"],
+            "handlers": ["daily_rotating_file", "error_file"],
+            "level": "INFO",
+            "propagate": True,
+        },
+        "root": {
+            "handlers": ["daily_rotating_file", "error_file"],
             "level": "INFO",
             "propagate": True,
         },
